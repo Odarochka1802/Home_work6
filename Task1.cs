7a863d31f58b4073715545bd49c4500ea7ff1546
Odarochka1802 <64232325+Odarochka1802@users.noreply.github.com>
2022-09-18 12:51:10 +0300

    Task1
    Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
    0, 7, 8, -2, -2 -> 2
    1, -7, 567, 89, 223-> 3

---
+++ Task1.cs
@@ -0,0 +1,29 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+Console.WriteLine("Введите числа через пробел: ");
+int[] array = Console.ReadLine().Split(' ').Select(Int32.Parse).ToArray();
+
+void possitiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"В массиве больше 0: {count} элемента");
+}
+
+
+try
+{
+    possitiveNumbers(array);
+}
+catch (FormatException)
+{
+    Console.WriteLine("Неверный формат ввода!");
+}
